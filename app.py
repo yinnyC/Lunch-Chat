2635f7489ec3f3477266e77e95a196a0d7413a4b
Yin <yinnnn17@gmail.com>
2021-02-02 21:51:23 -0800

    Finished Sign up page

--- app.py
+++ app.py
@@ -16,7 +16,14 @@ app = Flask(__name__)  # Instantiate the Flask class
 def homepage():
     """Return index template."""
     sth = "hello world"
-    return render_template('index.html', sth=sth)
+    # return render_template('index.html', sth=sth)
+    return render_template('signup.html')
+
+
+@app.route('/signup')
+def signup_page():
+    """Return signup template."""
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
